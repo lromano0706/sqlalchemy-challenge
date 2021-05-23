116f1c5fa597418123366c519ee89ee0c2481a19
lromano0706 <78659955+lromano0706@users.noreply.github.com>
2021-05-23 12:59:04 -0700

    uploaded

--- Climate_app.py
+++ Climate_app.py
@@ -35,7 +35,6 @@ def Home_Page():
         f"/api/v1.0/precipitation<br/>"
         f"/api/v1.0/stations<br/>"
         f"/api/v1.0/tobs<br/>"
-        f"/api/v1.0/passengers<br/>"
         f"/api/v1.0/&lt;start&gt; where start is a date in YYYY-MM-DD format <br>" 
         f"/api/v1.0/&lt;start&gt;/&lt;end&gt; where start and end are dates in YYYY-MM-DD format"
         
@@ -66,18 +65,50 @@ def Stations():
     session = Session(engine)
     # Query all stations
     results = session.query(Station.station).all() 
+    
+    session.close()
     # Convert list of tuples into normal list
     all_stations = list(np.ravel(results))
 
     return jsonify(all_stations)
-   
 
 
+@Climate_app.route("/api/v1.0/tobs")
+def tobs():
+    # Create our session (link) from Python to the DB
+    session = Session(engine)
+    query_date = dt.date(2017,8,23) - dt.timedelta(days=365)
+    # Query all tobs
+    results = session.query(Measurement.tobs).filter(Measurement.station == "USC00519281").\
+    filter(Measurement.date >= query_date).all() 
+    
+    session.close()
+    
+    most_active = list(np.ravel(results))
+    return jsonify(most_active)
 
+@Climate_app.route("/api/v1.0/<start>")
+def start_date(start):
+    # Create our session (link) from Python to the DB
+    session = Session(engine)
+    # Query given start and calculating min avg max for all dates greater than and equal to th start date.
+    start_date = session.query(Measurement.date, func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)).\
+    filter(Measurement.date >= start).group_by(Measurement.date).all()
+    # Convert list of tuples into normal list
+    start_date_list = list(np.ravel(start_date))
+    return jsonify(start_date_list)
 
-
-
-
+@Climate_app.route("/api/v1.0/<start>/<end>")
+def start_end_date(start,end):
+    # Create our session (link) from Python to the DB
+    session = Session(engine)
+    # Query given start and calculating min avg max for all dates greater than and equal to th start date and end date
+    start_end_day = session.query(Measurement.date, func.min(Measurement.tobs), func.avg(Measurement.tobs),func.max(Measurement.tobs)).\
+    filter(Measurement.date >= start).filter(Measurement.date <= end).group_by(Measurement.date).all()
+    
+    session.close()
+        
+    return jsonify(start_end_day)    
 
 if __name__ == '__main__':
     Climate_app.run(debug=True)
